9d4f732b226d212542c7ad16428179e729dc6aa1
RodrigoSouzaa <contato.rodrigoom@gmail.com>
2022-05-22 21:14:02 -0300

    Aplicacao de instanciacao no arquivo usuarioRepository

--- src/main/java/com/ProjetoSpringBoot/ProjetoSpringBootApplication.java
+++ src/main/java/com/ProjetoSpringBoot/ProjetoSpringBootApplication.java
@@ -1,13 +1,33 @@
 package com.ProjetoSpringBoot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSpringBootApplication {
+import com.ProjetoSpringBoot.domain.Usuario;
+import com.ProjetoSpringBoot.repositories.UsuarioRepository;
 
+@SpringBootApplication
+public class ProjetoSpringBootApplication implements CommandLineRunner {
+	
+	@Autowired   
+	private UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Usuario novoUsuario = new Usuario(null, "Rodrigo", "Meuemail@gmail.com" );
+		Usuario segundoUsuario = new Usuario(null, "Souza", "Esseemail@gmail.com" );
+		
+		usuarioRepository.saveAll(Arrays.asList(novoUsuario,segundoUsuario));
+		
 	}
  
 }
